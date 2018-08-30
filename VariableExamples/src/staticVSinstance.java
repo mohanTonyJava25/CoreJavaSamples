4efca33806f30fcd182a4d80902fadd671c4a52d
mohanTonyJava25 <mohan.paralla@hotmail.com>
2018-08-30 12:52:01 -0400

    updated

--- VariableExamples/src/staticVSinstance.java
+++ VariableExamples/src/staticVSinstance.java
@@ -1,32 +1,28 @@
 
 public class staticVSinstance {
 
-	int n = 1;
-	static int k = 1;
-	
-	staticVSinstance()
-	{
+	int n = 1; // instance variable
+	static int k = 1; //static variable
+
+	staticVSinstance() {
 		System.out.println(n);
 		System.out.println(k);
 		System.out.println("inside a constructor");
-		n ++;
+		n++;
 		k++;
 		System.out.println(n);
 		System.out.println(k);
 	}
-	
+
 	public static void main(String[] args) {
 
-		
-		staticVSinstance SI = new staticVSinstance();
+		staticVSinstance SI = new staticVSinstance(); // reference variable SI is referencing the class staticVSinstance
 		staticVSinstance SI1 = new staticVSinstance();
 		staticVSinstance SI2 = new staticVSinstance();
-		
-		System.out.println( "this is n value\t" +SI.n);
-		System.out.println(" this is k value\t" +SI.k);
-		
-		
+
+		System.out.println("this is n value\t" + SI.n);
+		System.out.println(" this is k value\t" + SI.k);
+
 	}
-	
-	
+
 }
